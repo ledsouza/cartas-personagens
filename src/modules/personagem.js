159812d5fed645c2fdbf19a86fdbf18dad87ae74
ledsouza <leandro.souza.159@gmail.com>
2024-05-30 10:29:47 -0300

    refatorado Personagem implementado novo método em Mago

--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -12,8 +12,8 @@ export class Personagem {
 
     obterInsignia() {
         if (this.level >= 5) {
-            return "Implacavel ${this.tipo}";
+            return `Implacavel ${this.tipo}`;
         }
-        return "${this.tipo} iniciante";
+        return `${this.tipo} iniciante`;
     }
 }
