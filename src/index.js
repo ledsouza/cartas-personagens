723c9213241a62e0e1c23875248c151ed7748cf6
ledsouza <leandro.souza.159@gmail.com>
2024-05-30 10:34:34 -0300

    implementada classe Arqueiro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { Personagem } from "./modules/personagem.js";
 import { personagemView } from "./components/personagem-view.js";
 import { Mago } from "./modules/mago.js";
+import { Arqueiro } from "./modules/arqueiro.js";
 
 const magoAntonio = new Mago("Antonio", 4, "fogo", 4, 3);
 const magaJulia = new Mago("Julia", 8, "gelo", 7, 10);
+const arqueiroFernando = new Arqueiro("Fernando", 10, 12);
 
-const personagens = [magoAntonio, magaJulia];
+const personagens = [magoAntonio, magaJulia, arqueiroFernando];
 
 new personagemView(personagens).render();
